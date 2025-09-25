7b26475fbdabfaefeb98c1d025dcc508c667f458
Sungho95 <80394894+Sungho95@users.noreply.github.com>
2025-09-25 21:56:10 +0900

    [D3] Title: N-Queen, Time: 86 ms, Memory: 25,600 KB -BaekjoonHub

--- SWEA/D3/2806. N－Queen/N－Queen.java
+++ SWEA/D3/2806. N－Queen/N－Queen.java
@@ -1,46 +1,54 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
-class Solution {
-    private static int n;
-    private static int[] board;
-    private static int count;
+public class Solution {
 
-    public static void main(String args[]) throws Exception {
+	static int n;
+	static int[] board;
+	static int count;
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
+	public static void main(String[] args) throws IOException {
 
-        for (int test_case = 1; test_case <= T; test_case++) {
-            n = Integer.parseInt(br.readLine());
-            board = new int[n];
-            count = 0;
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-            nQueen(n, 0);
+		int t = Integer.parseInt(br.readLine());
+		for (int tc = 1; tc <= t; tc++) {
+			n = Integer.parseInt(br.readLine());
+			board = new int[n];
+			count = 0;
 
-            System.out.println("#" + test_case + " " + count);
-        }
-    }
+			nQueen(n, 0);
 
-    private static void nQueen(int n, int start) {
-        if (start == n) {
-            count++;
-            return;
-        }
+			bw.write("#" + tc + " " + count + "\n");
+			bw.flush();
+		}
+		br.close();
+		bw.close();
+	}
 
-        for (int x = 0; x < n; x++) {
-            boolean isPossible = true;
-            for (int y = 0; y < start; y++) {
-                if (x == board[y] || x == board[y] + (start - y) || x == board[y] - (start - y)) {
-                    isPossible = false;
-                    break;
-                }
-            }
+	private static void nQueen(final int n, final int start) {
+		if (start == n) {
+			count++;
+			return;
+		}
 
-            if (isPossible) {
-                board[start] = x;
-                nQueen(n, start + 1);
-            }
-        }
-    }
+		for (int i = 0; i < n; i++) {
+			boolean flag = true;
+			for (int j = 0; j < start; j++) {
+				if (i == board[j] || i == board[j] + start - j || i == board[j] - start + j) {
+					flag = false;
+					break;
+				}
+			}
+
+			if (flag) {
+				board[start] = i;
+				nQueen(n, start + 1);
+			}
+		}
+	}
 }
