7085a029127306938a9a1f18efee85b98a4aad7b
Sungho95 <80394894+Sungho95@users.noreply.github.com>
2025-09-25 21:37:48 +0900

    [D3] Title: 팰린드롬 문제, Time: 159 ms, Memory: 46,068 KB -BaekjoonHub

--- SWEA/D3/19003. 팰린드롬 문제/팰린드롬 문제.java
+++ SWEA/D3/19003. 팰린드롬 문제/팰린드롬 문제.java
@@ -1,51 +1,67 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.*;
+import java.util.*;
 
-public class Solution {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
+class Solution {
+	public static void main(String args[]) throws Exception {
+		// System.setIn(new FileInputStream("src/input.txt"));
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		sc.nextLine();
 
-        for (int test_case = 1; test_case <= T; test_case++) {
-            String[] input = br.readLine().split(" ");
-            int N = Integer.parseInt(input[0]);
-            int M = Integer.parseInt(input[1]);
-            int length;
-            int count = 0;
-            List<String> isNotPalindrome = new ArrayList<>();
+		for (int test_case = 1; test_case <= T; test_case++) {
+			String[] input = sc.nextLine().split(" ");
+			int N = Integer.parseInt(input[0]);
+			int M = Integer.parseInt(input[1]);
+			int result = 0;
 
-            boolean hasPalindrome = false;
-            for (int i = 0; i < N; i++) {
-                String word = br.readLine();
-                if (isPalindrome(word)) {
-                    hasPalindrome = true;
-                } else {
-                    isNotPalindrome.add(word);
-                }
-            }
+			Set<String> set = new HashSet<>();
+			boolean flag = false;
+			for (int i = 0; i < N; i++) {
+				String word = sc.nextLine();
+				if (isPalindrome(word)) {
+					flag = true;
+				} else {
+					String reverse = reverse(word);
+					if (set.contains(reverse)) {
+						result += M * 2;
+					} else {
+						set.add(word);
+					}
+				}
+			}
 
-            for (String word : isNotPalindrome) {
-                String reverse = new StringBuilder(word).reverse().toString();
-                if (isNotPalindrome.contains(reverse)) {
-                    count++;
-                }
-            }
+			if (flag) {
+				result += M;
+			}
 
-            if (hasPalindrome) {
-                length = count * M + M;
-            } else {
-                length = count * M;
-            }
+			System.out.println("#" + test_case + " " + result);
+		}
+	}
 
-            System.out.println("#" + test_case + " " + length);
-        }
-    }
-    
-    private static boolean isPalindrome(String word) {
-        StringBuilder reverse = new StringBuilder(word).reverse();
-        return reverse.toString().equals(word);
-    }
+	// 문자열 뒤집기
+	private static String reverse(String word) {
+		char[] arr = word.toCharArray();
+		int start = 0;
+		int end = arr.length - 1;
+		while (start < end) {
+			char temp = arr[start];
+			arr[start] = arr[end];
+			arr[end] = temp;
+			start++;
+			end--;
+		}
+
+		return String.valueOf(arr);
+	}
+
+	// 팰린드롬인지 확인
+	private static boolean isPalindrome(String word) {
+		for (int i = 0; i < word.length() / 2; i++) {
+			if (word.charAt(i) != word.charAt(word.length() - 1 - i)) {
+				return false;
+			}
+		}
+
+		return true;
+	}
 }
